a62915f6bf5c8d9f811afd1baf7e6b53435de0d1
Jason Lingle <jason@lin.gl>
2020-07-10 02:25:01 +0000

    Add append command parts to command processor.

--- src/imap/command_processor/messages.rs
+++ src/imap/command_processor/messages.rs
@@ -17,6 +17,9 @@
 // Crymap. If not, see <http://www.gnu.org/licenses/>.
 
 use std::borrow::Cow;
+use std::io::Read;
+
+use chrono::prelude::*;
 
 use super::defs::*;
 use crate::account::mailbox::{StatefulMailbox, StatelessMailbox};
@@ -24,6 +27,119 @@ use crate::account::model::*;
 use crate::support::error::Error;
 
 impl CommandProcessor {
+    /// Start an append command.
+    ///
+    /// This call adds the first item to the append. Further items are added by
+    /// subsequent calls to `cmd_append_item()`. Once everything is done,
+    /// `cmd_append_commit()` is called to actually run the commit.
+    ///
+    /// If `cmd_append_start()` or `cmd_append_item()` fails,
+    /// `cmd_append_abort()` must be called, and then the error response from
+    /// the failing command returned with the tag matching the original
+    /// request.
+    pub(super) fn cmd_append_start(
+        &mut self,
+        cmd: s::AppendCommandStart<'_>,
+        item_size: u32,
+        item_data: impl Read,
+    ) -> PartialResult<()> {
+        let dst = account!(self)?.mailbox(&cmd.mailbox, false).map_err(
+            map_error! {
+                self,
+                NxMailbox | UnsafeName | MailboxUnselectable => (No, None),
+            },
+        )?;
+        self.multiappend = Some(Multiappend {
+            dst,
+            request: AppendRequest { items: vec![] },
+        });
+        self.cmd_append_item(cmd.first_fragment, item_size, item_data)
+    }
+
+    pub(super) fn cmd_append_item(
+        &mut self,
+        cmd: s::AppendFragment<'_>,
+        item_size: u32,
+        item_data: impl Read,
+    ) -> PartialResult<()> {
+        if 0 == item_size {
+            return Err(s::Response::Cond(s::CondResponse {
+                cond: s::RespCondType::No,
+                code: None,
+                quip: Some(Cow::Borrowed("APPEND aborted by empty payload")),
+            }));
+        }
+
+        let append = self
+            .multiappend
+            .as_mut()
+            .expect("cmd_append_item with no append in progress");
+
+        if append.request.items.len() >= 65536 {
+            return Err(s::Response::Cond(s::CondResponse {
+                cond: s::RespCondType::No,
+                code: None,
+                quip: Some(Cow::Borrowed(
+                    "Maximum message count for MULTIAPPEND is 65536",
+                )),
+            }));
+        }
+
+        let buffered = append
+            .dst
+            .buffer_message(
+                cmd.internal_date.unwrap_or_else(|| {
+                    FixedOffset::east(0)
+                        .from_utc_datetime(&Utc::now().naive_local())
+                }),
+                item_data,
+            )
+            .map_err(map_error!(self))?;
+        append.request.items.push(AppendItem {
+            buffer_file: buffered,
+            flags: cmd.flags.unwrap_or_default(),
+        });
+
+        Ok(())
+    }
+
+    pub(super) fn cmd_append_commit(
+        &mut self,
+        tag: Cow<'static, str>,
+        sender: SendResponse<'_>,
+    ) -> s::ResponseLine<'static> {
+        let append = self
+            .multiappend
+            .take()
+            .expect("cmd_append_commit with no append in progress");
+        match append.dst.multiappend(append.request).map_err(map_error! {
+            self,
+            MailboxFull | GaveUpInsertion => (No, None),
+        }) {
+            Ok(_appended) => { /* TODO UIDPLUS */ }
+            Err(response) => {
+                return s::ResponseLine {
+                    tag: Some(tag),
+                    response,
+                };
+            }
+        }
+
+        // Fall through to a NOOP to get the rest of the post-command
+        // semantics.
+        self.handle_command(
+            s::CommandLine {
+                tag,
+                cmd: s::Command::Simple(s::SimpleCommand::Noop),
+            },
+            sender,
+        )
+    }
+
+    pub(super) fn cmd_append_abort(&mut self) {
+        self.multiappend = None;
+    }
+
     pub(super) fn cmd_expunge(
         &mut self,
         _sender: SendResponse<'_>,
