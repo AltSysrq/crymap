e8df45b60985954a8f143309bb6ae374de4bdca7
Jason Lingle <jason@lin.gl>
2023-12-15 01:13:03 +0000

    Port V1 expunge tests to V2.

--- src/account/v2/state/expunge.rs
+++ src/account/v2/state/expunge.rs
@@ -16,8 +16,15 @@
 // You should have received a copy of the GNU General Public License along with
 // Crymap. If not, see <http://www.gnu.org/licenses/>.
 
+use chrono::prelude::*;
+use log::warn;
+
+use super::super::storage;
 use super::defs::*;
-use crate::{account::model::*, support::error::Error};
+use crate::{
+    account::model::*,
+    support::{chronox::*, error::Error},
+};
 
 impl Account {
     /// Directly expunge the given UIDs, without going through the \Deleted
@@ -43,7 +50,8 @@ impl Account {
     /// This is the `UID EXPUNGE` operation from RFC 4315.
     ///
     /// Checking the `\Deleted` flag is done with respect to the current
-    /// snapshot and not the true message state.
+    /// snapshot and not the true message state, which makes it easier for the
+    /// client to predict what the command will actually do.
     ///
     /// There is no error if `uids` includes a non-addressable UID. RFC 4315
     /// does not explicitly describe any particular behaviour when the client
@@ -105,6 +113,263 @@ impl Account {
         )?;
         Ok(())
     }
+
+    /// Immediately purge all pending soft expunges which have a
+    /// `last_activity` before the given datetime.
+    ///
+    /// This is normally used as part of periodic messages to remove unused
+    /// message files, but is also used for testing what happens when a removed
+    /// message is read.
+    ///
+    /// Returns the number of messages that were purged.
+    pub fn purge(&mut self, dt: DateTime<Utc>) -> Result<u32, Error> {
+        let orphans = self
+            .metadb
+            .fetch_orphaned_messages(storage::UnixTimestamp(dt))?;
+
+        let mut removed = 0u32;
+        for &(message, ref path) in &orphans {
+            // Delete the message itself first so that it will never get
+            // "recovered" into the INBOX.
+            if let Err(e) = self.message_store.delete(path.as_ref()) {
+                // TODO log prefix
+                warn!("delete message at {path:?}: {e:?}");
+            } else {
+                self.metadb.forget_message(message)?;
+                removed += 1;
+            }
+        }
+
+        Ok(removed)
+    }
+
+    /// Convenience for calling `purge()` with a date far in the future.
+    pub fn purge_all(&mut self) -> Result<u32, Error> {
+        self.purge(NaiveDate::from_ymdx(9999, 12, 31).and_hmsx_utc(23, 59, 59))
+    }
 }
 
-// TODO Adapt tests from v1/mailbox/expunge.rs
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn expunge_of_expunged_message_succeeds_quietly() {
+        let mut fixture = TestFixture::new();
+        let (mut mb1, _) = fixture.account.select("INBOX", true, None).unwrap();
+        let (mut mb2, _) = fixture.account.select("INBOX", true, None).unwrap();
+
+        // Create a message with the \Deleted flag set
+        let uid = fixture.simple_append("INBOX");
+        fixture.account.poll(&mut mb1).unwrap();
+        fixture
+            .account
+            .store(
+                &mut mb1,
+                &StoreRequest {
+                    ids: &SeqRange::just(uid),
+                    flags: &[Flag::Deleted],
+                    remove_listed: false,
+                    remove_unlisted: false,
+                    loud: false,
+                    unchanged_since: None,
+                },
+            )
+            .unwrap();
+        fixture.account.poll(&mut mb1).unwrap();
+
+        // Let mb2 see it too
+        fixture.account.poll(&mut mb2).unwrap();
+
+        // Expunge it in mb1 and verify it's gone.
+        fixture.account.expunge_all_deleted(&mb1).unwrap();
+        let poll = fixture.account.poll(&mut mb1).unwrap();
+        assert_eq!(vec![(Seqnum::u(1), uid)], poll.expunge);
+        assert_eq!(None, mb1.uid_index(uid));
+
+        // Expunge via mb2, which thinks the message still exists.
+        fixture.account.expunge_all_deleted(&mb2).unwrap();
+        let poll = fixture.account.poll(&mut mb2).unwrap();
+        assert_eq!(vec![(Seqnum::u(1), uid)], poll.expunge);
+        assert_eq!(None, mb2.uid_index(uid));
+
+        // Also ensure that the second expunge doesn't break mb1
+        fixture.account.poll(&mut mb1).unwrap();
+    }
+
+    #[test]
+    fn expunge_empty_mailbox() {
+        let mut fixture = TestFixture::new();
+        let (mut mb1, _) = fixture.account.select("INBOX", true, None).unwrap();
+        fixture.account.expunge_all_deleted(&mb1).unwrap();
+        fixture.account.poll(&mut mb1).unwrap();
+    }
+
+    // v1/mailbox/expunge.rs has a test
+    //   fn expunge_sees_delete_set_in_other_session()
+    // which sets \Deleted in one session, then invokes EXPUNGE in a second
+    // session without first polling to allow that second session to discover
+    // the flag. This is an expected behaviour difference in V2.
+
+    #[test]
+    fn expunge_doesnt_see_delete_set_in_other_session() {
+        let mut fixture = TestFixture::new();
+        let uid = fixture.simple_append("INBOX");
+        let (mut mb1, _) = fixture.account.select("INBOX", true, None).unwrap();
+        let (mut mb2, _) = fixture.account.select("INBOX", true, None).unwrap();
+
+        // Session 1 sets \Deleted
+        fixture
+            .account
+            .store(
+                &mut mb1,
+                &StoreRequest {
+                    ids: &SeqRange::just(uid),
+                    flags: &[Flag::Deleted],
+                    remove_listed: false,
+                    remove_unlisted: false,
+                    loud: false,
+                    unchanged_since: None,
+                },
+            )
+            .unwrap();
+
+        // Session 2 runs EXPUNGE without having a chance to see the flag
+        // change.
+        fixture.account.expunge_all_deleted(&mb2).unwrap();
+        // In the end, nothing is expunged.
+        let poll = fixture.account.poll(&mut mb2).unwrap();
+        assert!(poll.expunge.is_empty());
+    }
+
+    #[test]
+    fn expunge_wont_delete_invisible_messages() {
+        let mut fixture = TestFixture::new();
+        let uid1 = fixture.simple_append("INBOX");
+        let (mut mb1, _) = fixture.account.select("INBOX", true, None).unwrap();
+        let (mut mb2, _) = fixture.account.select("INBOX", true, None).unwrap();
+
+        // A second STORE comes in, and the first session sees that message and
+        // sets \Deleted on both messages.
+        let uid2 = fixture.simple_append("INBOX");
+        fixture.account.poll(&mut mb1).unwrap();
+        fixture
+            .account
+            .store(
+                &mut mb1,
+                &StoreRequest {
+                    ids: &SeqRange::range(uid1, uid2),
+                    flags: &[Flag::Deleted],
+                    remove_listed: false,
+                    remove_unlisted: false,
+                    loud: false,
+                    unchanged_since: None,
+                },
+            )
+            .unwrap();
+
+        // The second session does a mini poll, which exposes it to the new
+        // \Deleted state.
+        assert_eq!(1, fixture.account.mini_poll(&mut mb2).unwrap().fetch.len());
+        // When the second session runs EXPUNGE, only the message actually in
+        // its snapshot is removed.
+        fixture.account.expunge_all_deleted(&mb2).unwrap();
+
+        let poll = fixture.account.poll(&mut mb1).unwrap();
+        assert_eq!(vec![(Seqnum::u(1), uid1)], poll.expunge);
+    }
+
+    #[test]
+    fn uid_expunge_wont_delete_invisible_messages() {
+        let mut fixture = TestFixture::new();
+        let uid1 = fixture.simple_append("INBOX");
+        let (mut mb1, _) = fixture.account.select("INBOX", true, None).unwrap();
+        let (mut mb2, _) = fixture.account.select("INBOX", true, None).unwrap();
+
+        // A second STORE comes in, and the first session sees that message and
+        // sets \Deleted on both messages.
+        let uid2 = fixture.simple_append("INBOX");
+        fixture.account.poll(&mut mb1).unwrap();
+        fixture
+            .account
+            .store(
+                &mut mb1,
+                &StoreRequest {
+                    ids: &SeqRange::range(uid1, uid2),
+                    flags: &[Flag::Deleted],
+                    remove_listed: false,
+                    remove_unlisted: false,
+                    loud: false,
+                    unchanged_since: None,
+                },
+            )
+            .unwrap();
+
+        // The second session does a mini poll, which exposes it to the new
+        // \Deleted state.
+        assert_eq!(1, fixture.account.mini_poll(&mut mb2).unwrap().fetch.len());
+        // When the second session runs UID EXPUNGE, explicitly requesting both
+        // UIDs, only the message actually in its snapshot is removed.
+        fixture
+            .account
+            .expunge_deleted(&mb2, &SeqRange::range(uid1, uid2))
+            .unwrap();
+
+        let poll = fixture.account.poll(&mut mb1).unwrap();
+        assert_eq!(vec![(Seqnum::u(1), uid1)], poll.expunge);
+    }
+
+    #[test]
+    fn no_expunge_on_read_only() {
+        let mut fixture = TestFixture::new();
+        let (mb, _) = fixture.account.select("INBOX", false, None).unwrap();
+
+        assert_matches!(
+            Err(Error::MailboxReadOnly),
+            fixture
+                .account
+                .expunge_deleted(&mb, &SeqRange::just(Uid::MIN)),
+        );
+        assert_matches!(
+            Err(Error::MailboxReadOnly),
+            fixture.account.expunge_all_deleted(&mb),
+        );
+        assert_matches!(
+            Err(Error::MailboxReadOnly),
+            fixture.account.vanquish(&mb, &SeqRange::just(Uid::MIN)),
+        );
+    }
+
+    #[test]
+    fn no_error_on_uid_expunge_with_bad_uids() {
+        let mut fixture = TestFixture::new();
+        for _ in 0..3 {
+            fixture.simple_append("INBOX");
+        }
+        let (mut mb, _) = fixture.account.select("INBOX", true, None).unwrap();
+
+        fixture
+            .account
+            .store(
+                &mut mb,
+                &StoreRequest {
+                    ids: &SeqRange::just(Uid::u(2)),
+                    flags: &[Flag::Deleted],
+                    remove_listed: false,
+                    remove_unlisted: false,
+                    loud: false,
+                    unchanged_since: None,
+                },
+            )
+            .unwrap();
+        fixture.account.poll(&mut mb).unwrap();
+
+        fixture.account.expunge_all_deleted(&mb).unwrap();
+        fixture.account.poll(&mut mb).unwrap();
+
+        fixture
+            .account
+            .expunge_deleted(&mb, &SeqRange::range(Uid::u(1), Uid::u(5)))
+            .unwrap();
+    }
+}
