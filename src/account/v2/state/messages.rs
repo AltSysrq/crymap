3546b49da5aa1ae0fb196813c2c3e862d031a882
Jason Lingle <jason@lin.gl>
2023-12-16 23:21:23 +0000

    Implement V2 COPY and MOVE.

--- src/account/v2/state/messages.rs
+++ src/account/v2/state/messages.rs
@@ -185,4 +185,449 @@ impl Account {
 
         Ok((mailbox_id.as_uid_validity()?, first_uid))
     }
+
+    /// The RFC 3501 `COPY` command.
+    pub fn seqnum_copy(
+        &mut self,
+        mb: &Mailbox,
+        request: &CopyRequest<Seqnum>,
+        dst: &str,
+    ) -> Result<CopyResponse, Error> {
+        self.copy(
+            mb,
+            &CopyRequest {
+                ids: mb.seqnum_range_to_uid(&request.ids, false)?,
+            },
+            dst,
+        )
+    }
+
+    /// The RFC 3501 `UID COPY` command.
+    pub fn copy(
+        &mut self,
+        mb: &Mailbox,
+        request: &CopyRequest<Uid>,
+        dst: &str,
+    ) -> Result<CopyResponse, Error> {
+        let dst_id = self.metadb.find_mailbox(dst)?;
+        let from_uids = mb.filter_uid_range(&request.ids);
+        let ret = self.metadb.copy_mailbox_messages(
+            mb.id,
+            &mut from_uids.items(u32::MAX),
+            dst_id,
+        );
+        ret
+    }
+
+    /// The RFC 3501 `MOVE` command.
+    pub fn seqnum_moove(
+        &mut self,
+        mb: &Mailbox,
+        request: &CopyRequest<Seqnum>,
+        dst: &str,
+    ) -> Result<CopyResponse, Error> {
+        self.moove(
+            mb,
+            &CopyRequest {
+                ids: mb.seqnum_range_to_uid(&request.ids, false)?,
+            },
+            dst,
+        )
+    }
+
+    /// The RFC 3501 `UID MOVE` command.
+    pub fn moove(
+        &mut self,
+        mb: &Mailbox,
+        request: &CopyRequest<Uid>,
+        dst: &str,
+    ) -> Result<CopyResponse, Error> {
+        mb.require_writable()?;
+        let dst_id = self.metadb.find_mailbox(dst)?;
+        let from_uids = mb.filter_uid_range(&request.ids);
+        let ret = self.metadb.move_mailbox_messages(
+            mb.id,
+            from_uids.items(u32::MAX),
+            dst_id,
+        );
+        ret
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::support::chronox::*;
+
+    #[test]
+    fn copy_into_self() {
+        let mut fixture = TestFixture::new();
+        let uid1 = fixture.simple_append("INBOX");
+        let uid2 = fixture.simple_append("INBOX");
+
+        let (mut mb, _) = fixture.select("INBOX", true, None).unwrap();
+        fixture
+            .store(
+                &mut mb,
+                &StoreRequest {
+                    ids: &SeqRange::just(uid1),
+                    flags: &[Flag::Answered],
+                    remove_listed: false,
+                    remove_unlisted: false,
+                    loud: false,
+                    unchanged_since: None,
+                },
+            )
+            .unwrap();
+        fixture
+            .store(
+                &mut mb,
+                &StoreRequest {
+                    ids: &SeqRange::just(uid2),
+                    flags: &[Flag::Draft],
+                    remove_listed: false,
+                    remove_unlisted: false,
+                    loud: false,
+                    unchanged_since: None,
+                },
+            )
+            .unwrap();
+
+        // Re-select as read-only to verify that the read-onliness doesn't
+        // impact our ability to use the mailbox as a destination.
+        mb = fixture.select("INBOX", false, None).unwrap().0;
+        let uids3 = fixture
+            .copy(
+                &mb,
+                &CopyRequest {
+                    ids: SeqRange::just(uid1),
+                },
+                "InBoX",
+            )
+            .unwrap()
+            .to_uids
+            .items(u32::MAX)
+            .collect::<Vec<_>>();
+        assert_eq!(1, uids3.len());
+
+        let poll = fixture.poll(&mut mb).unwrap();
+        assert_eq!(Some(3), poll.exists);
+        assert_eq!(0, poll.expunge.len());
+
+        assert!(mb.test_flag_o(&Flag::Answered, uids3[0]));
+        assert!(!mb.test_flag_o(&Flag::Draft, uids3[0]));
+
+        let uids4 = fixture
+            .seqnum_copy(
+                &mb,
+                &CopyRequest {
+                    ids: SeqRange::range(Seqnum::u(1), Seqnum::u(2)),
+                },
+                "INBOX",
+            )
+            .unwrap()
+            .to_uids
+            .items(u32::MAX)
+            .collect::<Vec<_>>();
+        assert_eq!(2, uids4.len());
+
+        let poll = fixture.poll(&mut mb).unwrap();
+        assert_eq!(Some(5), poll.exists);
+        assert_eq!(0, poll.expunge.len());
+
+        assert!(mb.test_flag_o(&Flag::Answered, uids4[0]));
+        assert!(!mb.test_flag_o(&Flag::Draft, uids4[0]));
+        assert!(!mb.test_flag_o(&Flag::Answered, uids4[1]));
+        assert!(mb.test_flag_o(&Flag::Draft, uids4[1]));
+    }
+
+    #[test]
+    fn copy_into_other() {
+        let mut fixture = TestFixture::new();
+        let uid1 = fixture.simple_append("INBOX");
+        let uid2 = fixture.simple_append("INBOX");
+
+        let (mut inbox, _) = fixture.select("INBOX", true, None).unwrap();
+        let (mut archive, _) = fixture.select("Archive", false, None).unwrap();
+
+        fixture
+            .store(
+                &mut inbox,
+                &StoreRequest {
+                    ids: &SeqRange::just(uid1),
+                    flags: &[Flag::Answered],
+                    remove_listed: false,
+                    remove_unlisted: false,
+                    loud: false,
+                    unchanged_since: None,
+                },
+            )
+            .unwrap();
+        fixture
+            .store(
+                &mut inbox,
+                &StoreRequest {
+                    ids: &SeqRange::just(uid2),
+                    flags: &[Flag::Draft],
+                    remove_listed: false,
+                    remove_unlisted: false,
+                    loud: false,
+                    unchanged_since: None,
+                },
+            )
+            .unwrap();
+        fixture.poll(&mut inbox).unwrap();
+
+        let uids3 = fixture
+            .copy(
+                &inbox,
+                &CopyRequest {
+                    ids: SeqRange::just(uid1),
+                },
+                "Archive",
+            )
+            .unwrap()
+            .to_uids
+            .items(u32::MAX)
+            .collect::<Vec<_>>();
+        assert_eq!(1, uids3.len());
+
+        let poll = fixture.poll(&mut archive).unwrap();
+        assert_eq!(Some(1), poll.exists);
+        assert_eq!(0, poll.expunge.len());
+
+        assert!(archive.test_flag_o(&Flag::Answered, uids3[0]));
+        assert!(!archive.test_flag_o(&Flag::Draft, uids3[0]));
+
+        let uids4 = fixture
+            .seqnum_copy(
+                &inbox,
+                &CopyRequest {
+                    ids: SeqRange::range(Seqnum::u(1), Seqnum::u(2)),
+                },
+                "Archive",
+            )
+            .unwrap()
+            .to_uids
+            .items(u32::MAX)
+            .collect::<Vec<_>>();
+        assert_eq!(2, uids4.len());
+
+        let poll = fixture.poll(&mut archive).unwrap();
+        assert_eq!(Some(3), poll.exists);
+        assert_eq!(0, poll.expunge.len());
+
+        assert!(archive.test_flag_o(&Flag::Answered, uids4[0]));
+        assert!(!archive.test_flag_o(&Flag::Draft, uids4[0]));
+        assert!(!archive.test_flag_o(&Flag::Answered, uids4[1]));
+        assert!(archive.test_flag_o(&Flag::Draft, uids4[1]));
+    }
+
+    #[test]
+    fn moove_into_other() {
+        let mut fixture = TestFixture::new();
+        let uid1 = fixture.simple_append("INBOX");
+        let uid2 = fixture.simple_append("INBOX");
+
+        let (mut inbox, _) = fixture.select("INBOX", true, None).unwrap();
+        let (mut archive, _) = fixture.select("Archive", false, None).unwrap();
+
+        fixture
+            .store(
+                &mut inbox,
+                &StoreRequest {
+                    ids: &SeqRange::just(uid1),
+                    flags: &[Flag::Answered],
+                    remove_listed: false,
+                    remove_unlisted: false,
+                    loud: false,
+                    unchanged_since: None,
+                },
+            )
+            .unwrap();
+        fixture
+            .store(
+                &mut inbox,
+                &StoreRequest {
+                    ids: &SeqRange::just(uid2),
+                    flags: &[Flag::Draft],
+                    remove_listed: false,
+                    remove_unlisted: false,
+                    loud: false,
+                    unchanged_since: None,
+                },
+            )
+            .unwrap();
+        fixture.poll(&mut inbox).unwrap();
+
+        let uids3 = fixture
+            .moove(
+                &inbox,
+                &CopyRequest {
+                    ids: SeqRange::just(uid1),
+                },
+                "Archive",
+            )
+            .unwrap()
+            .to_uids
+            .items(u32::MAX)
+            .collect::<Vec<_>>();
+        assert_eq!(1, uids3.len());
+
+        let poll = fixture.poll(&mut archive).unwrap();
+        assert_eq!(Some(1), poll.exists);
+        assert_eq!(0, poll.expunge.len());
+
+        assert!(archive.test_flag_o(&Flag::Answered, uids3[0]));
+        assert!(!archive.test_flag_o(&Flag::Draft, uids3[0]));
+
+        let poll = fixture.poll(&mut inbox).unwrap();
+        assert_eq!(Some(1), poll.exists);
+        assert_eq!(1, poll.expunge.len());
+
+        let uids4 = fixture
+            .seqnum_moove(
+                &inbox,
+                &CopyRequest {
+                    ids: SeqRange::just(Seqnum::u(1)),
+                },
+                "Archive",
+            )
+            .unwrap()
+            .to_uids
+            .items(u32::MAX)
+            .collect::<Vec<_>>();
+        assert_eq!(1, uids4.len());
+
+        let poll = fixture.poll(&mut archive).unwrap();
+        assert_eq!(Some(2), poll.exists);
+        assert_eq!(0, poll.expunge.len());
+
+        assert!(!archive.test_flag_o(&Flag::Answered, uids4[0]));
+        assert!(archive.test_flag_o(&Flag::Draft, uids4[0]));
+
+        let poll = fixture.poll(&mut inbox).unwrap();
+        assert_eq!(Some(0), poll.exists);
+        assert_eq!(1, poll.expunge.len());
+    }
+
+    #[test]
+    fn moove_into_self() {
+        let mut fixture = TestFixture::new();
+        let uid1 = fixture.simple_append("INBOX");
+
+        let (mb, _) = fixture.select("INBOX", true, None).unwrap();
+        assert_matches!(
+            Err(Error::MoveIntoSelf),
+            fixture.moove(
+                &mb,
+                &CopyRequest {
+                    ids: SeqRange::just(uid1),
+                },
+                "iNbOx",
+            ),
+        );
+        assert_matches!(
+            Err(Error::MoveIntoSelf),
+            fixture.seqnum_moove(
+                &mb,
+                &CopyRequest {
+                    ids: SeqRange::just(Seqnum::u(1)),
+                },
+                "iNbOx",
+            ),
+        );
+    }
+
+    #[test]
+    fn moove_from_readonly() {
+        let mut fixture = TestFixture::new();
+        let uid1 = fixture.simple_append("INBOX");
+
+        let (mb, _) = fixture.select("INBOX", false, None).unwrap();
+        assert_matches!(
+            Err(Error::MailboxReadOnly),
+            fixture.moove(
+                &mb,
+                &CopyRequest {
+                    ids: SeqRange::just(uid1),
+                },
+                "Archive",
+            ),
+        );
+        assert_matches!(
+            Err(Error::MailboxReadOnly),
+            fixture.seqnum_moove(
+                &mb,
+                &CopyRequest {
+                    ids: SeqRange::just(Seqnum::u(1)),
+                },
+                "Archive",
+            ),
+        );
+    }
+
+    #[test]
+    fn moove_nx() {
+        let mut fixture = TestFixture::new();
+        let uid1 = fixture.simple_append("INBOX");
+        let uid2 = fixture.simple_append("INBOX");
+        let uid3 = fixture.simple_append("INBOX");
+
+        let (mb1, _) = fixture.select("INBOX", true, None).unwrap();
+        fixture.vanquish(&mb1, &SeqRange::just(uid2)).unwrap();
+        // Don't poll --- the mailbox state should still think the message
+        // still exists, leaving it to the database layer to discover it
+        // doesn't.
+
+        let response = fixture
+            .moove(
+                &mb1,
+                &CopyRequest {
+                    ids: SeqRange::range(uid1, uid3),
+                },
+                "Archive",
+            )
+            .unwrap();
+
+        assert_eq!(2, response.from_uids.len());
+        assert_eq!(2, response.to_uids.len());
+    }
+
+    #[test]
+    fn test_multiappend() {
+        let mut fixture = TestFixture::new();
+        let internal_date =
+            FixedOffset::zero().from_utc_datetime(&Utc::now().naive_local());
+
+        let mut append_request = AppendRequest::default();
+        append_request.items.push(AppendItem {
+            buffer_file: fixture
+                .buffer_message(internal_date, b"foo" as &[u8])
+                .unwrap(),
+            flags: vec![Flag::Answered],
+        });
+        append_request.items.push(AppendItem {
+            buffer_file: fixture
+                .buffer_message(internal_date, b"bar" as &[u8])
+                .unwrap(),
+            flags: vec![Flag::Draft],
+        });
+
+        let uids = fixture
+            .multiappend("INBOX", append_request)
+            .unwrap()
+            .uids
+            .items(u32::MAX)
+            .collect::<Vec<_>>();
+        assert_eq!(2, uids.len());
+
+        let (mb, _) = fixture.select("INBOX", false, None).unwrap();
+        assert_eq!(2, mb.select_response().unwrap().exists);
+
+        assert!(mb.test_flag_o(&Flag::Answered, uids[0]));
+        assert!(!mb.test_flag_o(&Flag::Answered, uids[1]));
+        assert!(!mb.test_flag_o(&Flag::Draft, uids[0]));
+        assert!(mb.test_flag_o(&Flag::Draft, uids[1]));
+    }
 }
